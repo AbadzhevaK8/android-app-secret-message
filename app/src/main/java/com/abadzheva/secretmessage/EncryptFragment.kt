65a9ea9f9287310bd5de0cb0386019c01652f356
k8abadzheva <make.me.loco@gmail.com>
2024-11-03 00:31:07 +0300

    feat: add EncryptFragment and navigation
    Added a new EncryptFragment and corresponding layout file.
    The layout includes a TextView to display the encrypted message.
    Added navigation from MessageFragment to EncryptFragment using an action in the navigation graph.
    The MessageFragment now navigates to the EncryptFragment when the "Next" button is clicked.

---
+++ app/src/main/java/com/abadzheva/secretmessage/EncryptFragment.kt
@@ -0,0 +1,18 @@
+package com.abadzheva.secretmessage
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.abadzheva.secretmessage.databinding.FragmentEncryptBinding
+
+class EncryptFragment : Fragment(R.layout.fragment_encrypt) {
+    private val binding by viewBinding(FragmentEncryptBinding::bind)
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
--- app/src/main/java/com/abadzheva/secretmessage/MessageFragment.kt
+++ app/src/main/java/com/abadzheva/secretmessage/MessageFragment.kt
@@ -1,18 +1,22 @@
 package com.abadzheva.secretmessage
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.abadzheva.secretmessage.databinding.FragmentMessageBinding
 
-class MessageFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+class MessageFragment : Fragment(R.layout.fragment_message) {
+    private val binding by viewBinding(FragmentMessageBinding::bind)
+
+    override fun onViewCreated(
+        view: View,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_message, container, false)
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.next.setOnClickListener {
+            findNavController().navigate(R.id.action_messageFragment_to_encryptFragment)
+        }
     }
 }
